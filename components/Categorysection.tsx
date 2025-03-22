dbce1b44eff5ac23e6af7cd08c7d5fc590b44c97
Chathuranga-Senanayaka <senanayakacjs@gmail.com>
2025-03-22 23:08:33 +0530

    modified categorysection.tsx

--- components/Categorysection.tsx
+++ components/Categorysection.tsx
@@ -1,10 +1,22 @@
 import React from "react";
+import { postData } from "@/db/post";
 import Postcard from "./ui/postcard";
-
-const Categorysection = () => {
+const Categorysection = ({
+  type,
+}: {
+  type: "categories" | "explore" | "all";
+}) => {
+  const posts = type === "all" ? postData.allposts() : postData[type];
   return (
-    <div>
-      <Postcard />
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      {posts.map((post) => (
+        <Postcard
+          key={post.id}
+          title={post.title}
+          description={post.description}
+          image={post.image}
+        />
+      ))}
     </div>
   );
 };
